bcdd2b11a3086a7165481c783c28d0cdd003e350
Nikolay Rednov <red.niko@yandex.ru>
2020-04-03 14:38:35 +0400

    Second commit

--- ConsoleApp1/D3/Program.cs
+++ ConsoleApp1/D3/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
 
-namespace D3
+
+struct Complex
 {
-    class Program
+    public double im;
+    public double re;
+
+    public Complex Plus(Complex x)
     {
-        static void Main(string[] args)
+        Complex y;
+        y.im = im + x.im;
+        y.re = re + x.re;
+        return y;
+    }
+    public Complex Minus(Complex x)
+    {
+        Complex y;
+        y.im = im - x.im;
+        y.re = re - x.re;
+        return y;
+    }
+    public Complex Multi(Complex x)
+    {
+        Complex y;
+        y.im = re * x.im + im * x.re;
+        y.re = re * x.re - im * x.im;
+        return y;
+    }
+    public string ToString()
+    {
+        return re + "+" + im + "i";
+    }
+
+} // Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // С клавиатуры вводятся числа, пока не будет введен 0 (каждое число в новой строке). 
+        //Требуется подсчитать сумму всех нечетных положительных чисел. Сами числа и сумму вывести на экран, используя tryParse;
+        Console.WriteLine("Введите числа");
+        
+        bool isNum = int.TryParse(Console.ReadLine(), out  int x);
+        int result = 0;
+        
+        while (!isNum)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введено не число.");
+            isNum = int.TryParse(Console.ReadLine(), out  x);
+           
         }
+        int a = x;
+        while (a != 0)
+        {
+            if (a % 2 == 1) result += a;
+
+            a = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+        Console.WriteLine($"Сумма нечетных чисел {result}");
     }
 }
+
