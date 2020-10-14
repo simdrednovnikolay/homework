d45d5f7b5f608bf3a2f0ab7faaafa8ae4909993c
Nikolay Rednov <red.niko@yandex.ru>
2020-10-14 17:05:03 +0400

    Snake

---
+++ ConsoleApp1/Snake/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 5;
+            int y1 = 3;
+            char sym1 = '@';
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
+        }
+    }
+}
